d24837c332f52d2b230e49adce3ef80744a06e93
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2020-12-10 23:44:33 +0530

    Feature: Room Listing Working Okay

---
+++ src/main/java/com/lakhveerbawa/learningspring/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.lakhveerbawa.learningspring.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.lakhveerbawa.learningspring.data.entity.Room;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+
+	
+}
