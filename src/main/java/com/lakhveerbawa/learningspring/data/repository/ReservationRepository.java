cedf5b37363d31af49b52741c900a5ec303a52ba
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2020-12-11 18:59:10 +0530

    wip: service and domain created

--- src/main/java/com/lakhveerbawa/learningspring/data/repository/ReservationRepository.java
+++ src/main/java/com/lakhveerbawa/learningspring/data/repository/ReservationRepository.java
@@ -1,10 +1,12 @@
 package com.lakhveerbawa.learningspring.data.repository;
 
+import java.sql.Date;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.lakhveerbawa.learningspring.data.entity.Reservation;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
-	public String name();
+	Iterable<Reservation> findReservationByReservationDate(Date date);
 }
